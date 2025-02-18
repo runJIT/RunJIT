207ab84cb137cdc6805f6b794ba0a27637499c38
Rene Peuser <rene.peuser@hotmail.de>
2025-02-18 18:32:38 +0100

    add new entry point

---
+++ src/RunJit.Cli/RunJit/New/ServerlessMinimalApiProject/CodeGen/src/Project/Aws/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace $ProjectName$.Aws
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        /// <summary>
+        /// This method is used to configure the ASP.NET Core application.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Init(IWebHostBuilder builder)
+        {
+            // For a typical Startup.cs configuration:
+            builder.UseStartup<Program>();
+        }
+    }
+}
